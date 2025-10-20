dbdcac77728ef5af84f384412fa2a65320399729
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-10-20 13:50:57 +0100

    9

--- problems/9/grader.py
+++ problems/9/grader.py
@@ -1,17 +1,9 @@
 from submission import f
 
 
-def __normalise(obj):
-    if isinstance(obj, bool):
-        return int(obj)
-    else:
-        return obj
-
-
 __t = int(input())
 for __i in range(__t):
     __s = input()
     __import__('sys').stderr.write(f"TESTCASE {__i + 1}: {repr(__s)}\n")
     __out = f(__s)
-    __out = __normalise(__out)
     print(__out)
